fc38d3f4983ec390564c5b923b58db5fe527a9f2
Artus U <artusrock@hotmail.com>
2022-01-13 11:42:15 +0000

    basic user model and connecting cassandraDB

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-from . import config
+from cassandra.cqlengine.management import sync_table
+from . import config, db
+from .users.models import User
 
 app = FastAPI()
 settings = config.get_settings()
+
+DB_SESSION = None
+
+
+@app.on_event("startup")
+def on_startup():
+    print("hello world")
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
 
 
 @app.get("/")
