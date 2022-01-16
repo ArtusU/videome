c1ea22bd5196389938375ebdb6ef8bc99dfc54ad
Artus U <artusrock@hotmail.com>
2022-01-16 11:42:09 +0000

    base.html

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+import pathlib
+from pipes import Template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from cassandra.cqlengine.management import sync_table
 from . import config, db
 from .users.models import User
 
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+TEMPLATE_DIR = BASE_DIR / "templates"
+
 app = FastAPI()
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
 settings = config.get_settings()
 
 DB_SESSION = None
-
 
 @app.on_event("startup")
 def on_startup():
@@ -17,9 +24,14 @@ def on_startup():
     sync_table(User)
 
 
-@app.get("/")
-def homepage():
-    return {"hello": "world"}
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    context = {
+        "request": request,
+        "abc": 123
+    }
+    return templates.TemplateResponse("home.html", context)
+
 
 
 @app.get("/users")
