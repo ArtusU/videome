48c2dd6a6723ad97aa32f93a1e0a5b58a8882bb2
Artus U <artusrock@hotmail.com>
2022-01-23 11:06:34 +0000

    Login Fixes #13

--- app/shortcuts.py
+++ app/shortcuts.py
@@ -14,7 +14,6 @@ def render(request, template_name, context={}, status_code:int=200, cookies:dict
     ctx.update({"request": request})
     t = templates.get_template(template_name)
     html_str = t.render(ctx)
-    
     response = HTMLResponse(html_str, status_code=status_code)
     response.set_cookie(key='darkmode', value=1)
     if len(cookies.keys()) > 0:
@@ -25,4 +24,3 @@ def render(request, template_name, context={}, status_code:int=200, cookies:dict
     # for key in request.cookies.keys():
     #     response.delete_cookie(key)
     return response
-    #return templates.TemplateResponse(template_name, ctx, status_code=status_code)
