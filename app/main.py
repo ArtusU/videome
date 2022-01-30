ae6c6cadc326dbba963407d02cb2ad5a67e453f7
Artus U <artusrock@hotmail.com>
2022-01-30 08:48:17 +0000

    Routers for Sub Modules in FastAPI Fixes #29

--- app/main.py
+++ app/main.py
@@ -19,16 +19,16 @@ from .users.schemas import (
     UserSignupSchema
 )
 from .videos.models import Video
+from .videos.routers import router as video_router
 
 
+DB_SESSION = None
 BASE_DIR = pathlib.Path(__file__).resolve().parent # app/
-TEMPLATE_DIR = BASE_DIR / "templates"
 
 app = FastAPI()
 app.add_middleware(AuthenticationMiddleware, backend=JWTCookieBackend)
-templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+app.include_router(video_router)
 
-DB_SESSION = None
 
 from .handlers import * #noqa
 
---
+++ app/videos/routers.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+from app.shortcuts import render, redirect
+
+
+router = APIRouter(
+    prefix='/videos'
+)
+
+@router.get("/", response_class=HTMLResponse)
+def video_list_view(request: Request):
+    context = {}
+    return render(request, "videos/list.html", context)
+
+
+
+@router.get("/detail", response_class=HTMLResponse)
+def video_detail_view(request: Request):
+    return render(request, "videos/detail.html", {})
