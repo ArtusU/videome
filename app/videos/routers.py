05ce0a6fa46b53c7a6b7da3cda1af1221082e019
Artus U <artusrock@hotmail.com>
2022-02-01 10:27:09 +0000

    Video Create View Form Fixes #32

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Request
+from email.contentmanager import raw_data_manager
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 
+from app import utils
 from app.users.decorators import login_required
 from app.shortcuts import render, redirect
+
+from .schemas import VideoCreateSchema
 
 
 router = APIRouter(
@@ -11,9 +15,28 @@ router = APIRouter(
 
 
 @router.get("/create", response_class=HTMLResponse)
+@login_required
 def video_create_view(request: Request):
-    context = {}
-    return render(request, "videos/create.html", context)
+    return render(request, "videos/create.html", {})
+
+@router.post("/create", response_class=HTMLResponse)
+@login_required
+def video_create_post_view(request: Request, title: str=Form(...), url: str = Form(...)):
+    raw_data = {
+        "title": title,
+        "url": url,
+        "user_id": request.user.username
+    }
+    data, errors = utils.valid_schema_data_or_error(raw_data, VideoCreateSchema)
+    context = {
+        "data": data,
+        "errors": errors,
+        "url": url,
+    }
+    if len(errors) > 0:
+        return render(request, "videos/create.html", context, status_code=400)
+    redirect_path = data.get('path') or "/videos/create" 
+    return redirect(redirect_path)
 
 
 @router.get("/", response_class=HTMLResponse)
